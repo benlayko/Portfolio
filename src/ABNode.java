a7e120a44b95d2d8583cc4ed592d1ce829e6e2f3
benlayko <81981968+benlayko@users.noreply.github.com>
2022-03-29 14:50:14 -0400

    Delete src directory

--- src/ABNode.java
+++
@@ -1,8 +0,0 @@
-import org.nd4j.linalg.api.ndarray.INDArray;
-
-public interface ABNode {
-	public float getValue();
-	public INDArray getBoard();
-	public ABNode getSuccessor();
-	public int[] getChange();
-}
